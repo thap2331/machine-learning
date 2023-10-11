ea778d060eecfa6921717ab28dc24277023ddaab
suraj_dell_linux <surazmah@gmail.com>
2023-10-11 16:00:32 -0500

    add random forest info

--- topics/random-forest/random-forest.md
+++ topics/random-forest/random-forest.md
@@ -2,20 +2,22 @@
 
 ## Conceptual understanding
 - Videos
-    - This [video](https://www.youtube.com/watch?v=J4Wdy0Wc_xQ) by Josh Starmer is amazing. It gives you a clear understanding of what random forest technique.
+    - This [YouTube video](https://www.youtube.com/watch?v=J4Wdy0Wc_xQ) by Josh Starmer is amazing. It gives you a clear understanding of random forest technique.
 
 
 ## Projects
 
 - <b> Project 1 </b>
 
-Predict wine quality [project](https://nbviewer.org/github/ethen8181/machine-learning/blob/master/trees/random_forest.ipynb#Implementation) using random forest. This is a really good read from top to bottom, but if you want to just do random forest for now, I am posting a question and solution here (coming soon a new readme here).
+Predict wine quality [project](https://nbviewer.org/github/ethen8181/machine-learning/blob/master/trees/random_forest.ipynb#Implementation) using random forest. This is a really good read from top to Implementation for beginner. 
+- The project has tree, random forest with all features, random forest with one third of the features at every split, and more. If you want to just do random forest (one third of the features at every split), I am posting a question and solution here (coming soon a new readme here).
 
 &emsp; <i>Source: Github repo of [Ethen](https://github.com/ethen8181)</i>
 
 
-- <b> Project 2 </b>
 
-Predict iphone purchase given age, salary, and gender
+<b> Project 2 </b>
+
+Predict iPhone purchase based on age, salary, and gender
 
 &emsp; <i> Source: Thanks to a [Medium article](https://medium.com/analytics-vidhya/machine-learning-project-6-predict-salary-using-random-forest-regression-9a18f97c91e5) by Omair. However, the article is behind paywall. The [dataset](https://github.com/omairaasim/machine_learning/blob/master/project_16_random_forest_classifier/iphone_purchase_records.csv) and [python code](https://github.com/omairaasim/machine_learning/blob/master/project_16_random_forest_classifier/random_forest_classifier.py) are in github. </i>
---
+++ topics/random-forest/wine-quality-random-forest.py
@@ -0,0 +1,80 @@
+import pandas as pd
+import numpy as np
+
+from sklearn.metrics import mean_squared_error, accuracy_score
+from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.tree import DecisionTreeRegressor, DecisionTreeClassifier
+from sklearn.ensemble import RandomForestRegressor, ExtraTreesRegressor
+
+wine = pd.read_csv('https://archive.ics.uci.edu/ml/machine-learning-databases/wine-quality/winequality-white.csv', sep = ';')
+
+print(wine.columns)
+# train/test split the features and response column
+y = wine['quality'].values
+X = wine.drop('quality', axis = 1).values
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size = 0.2, random_state = 1234)
+
+print('dimension of the dataset: ', wine.shape)
+
+np.random.seed(1)
+nums = np.arange(1, 21)
+
+class RandomForest:
+    """
+    Regression random forest using scikit learn's 
+    decision tree as the base tree
+    
+    Parameters
+    ----------
+    n_estimators: int 
+        the number of trees that you're going built
+        on the bagged sample (you can even shutoff
+        the bagging procedure for some packages)
+    
+    max_features: int 
+        the number of features that you allow
+        when deciding which feature to split on 
+    
+    all the other parameters for a decision tree like
+    max_depth or min_sample_split also applies to Random Forest, 
+    it is just not used here as that is more
+    related to a single decision tree
+    """
+
+    def __init__(self, n_estimators, max_features):
+        self.n_estimators = n_estimators
+        self.max_features = max_features
+        
+    def fit(self, X, y):
+        # for each base-tree models:
+        # 1. draw bootstrap samples from the original data
+        # 2. train the tree model on that bootstrap sample, and
+        #    during training, randomly select a number of features to 
+        #    split on each node
+        self.estimators = []
+        for i in range(self.n_estimators):
+            boot = np.random.choice(y.shape[0], size = y.shape[0], replace = True)
+            X_boot, y_boot = X[boot], y[boot]
+            tree = DecisionTreeRegressor(max_features = self.max_features)
+            tree.fit(X_boot, y_boot)
+            self.estimators.append(tree)
+            
+        return self
+
+    def predict(self, X):
+        # for the prediction, we average the
+        # predictions made by each of the bagged tree
+        pred = np.empty((X.shape[0], self.n_estimators))
+        for i, tree in enumerate(self.estimators):
+            pred[:, i] = tree.predict(X)
+            
+        pred = np.mean(pred, axis = 1)
+        return pred
+    
+# random forest using a random one third of the features at every split
+rf = RandomForest(n_estimators = 50, max_features = 1 / 3)
+rf.fit(X_train, y_train)
+rf_y_pred = rf.predict(X_test)
+print("MSE measures how far predictions deviate from target values. Smaller MSE values are better, and 0.0 means perfect prediction.")
+print('Random Forest MSE',  mean_squared_error(y_test, rf_y_pred))
